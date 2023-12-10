5fd70c4af6aca10cc2e785d7ade4d8ec45087561
Kotori316 <Kotori316@gmail.com>
2023-12-10 15:39:13 +0900

    Init terraform

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "kotori316-tf-state"
+    prefix = "miniature-spoon-terraform"
+  }
+  required_version = "~> 1.6"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
+}
+
+data "cloudflare_zone" "zone" {
+  name = var.cloudflare_zone_name
+}
+
+resource "cloudflare_r2_bucket" "maven_bucket" {
+  account_id = data.cloudflare_zone.zone.account_id
+  name       = var.maven_name
+}
+
+resource "cloudflare_worker_script" "maven_viewer" {
+  account_id = data.cloudflare_zone.zone.account_id
+  content    = <<EOF
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+async function handleRequest(request) {
+  return new Response("No content", {
+    status: 400,
+    headers: {
+      'content-type': 'text/plain',
+    },
+  });
+}
+EOF
+  name       = var.maven_name
+
+  r2_bucket_binding {
+    bucket_name = cloudflare_r2_bucket.maven_bucket.name
+    name        = "MAVEN_BUCKET"
+  }
+}
+
+resource "cloudflare_worker_domain" "maven_viewer" {
+  account_id = data.cloudflare_zone.zone.account_id
+  hostname   = "maven.${var.cloudflare_zone_name}"
+  service    = cloudflare_worker_script.maven_viewer.name
+  zone_id    = data.cloudflare_zone.zone.id
+  environment = "production"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "cloudflare_token" {
+  type = string
+}
+
+variable "cloudflare_zone_name" {
+  type    = string
+  default = "kotori316.com"
+}
+
+variable "maven_name" {
+  type = string
+  default = "kotori316-maven"
+}
