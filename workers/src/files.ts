8b2dad52504efebcde43ad1932ffd78b3c1d7f81
Kotori316 <Kotori316@gmail.com>
2023-12-16 02:06:57 +0900

    Use hono helper to get mime type

--- workers/src/files.ts
+++ workers/src/files.ts
@@ -1,24 +1,20 @@
 import { PathObject, ScanListResult } from "./data";
+import * as mime from "hono/utils/mime";
 import path from "path-browserify";
 
 const knownMimeTypes = new Map([
-  [".jar", "application/java-archive"],
-  [".json", "application/json"],
   [".module", "application/json"],
   [".pom", "application/xml"],
-  [".xml", "application/xml"],
-  [".zip", "application/zip"],
   [".md5", "text/plain"],
   [".sha1", "text/plain"],
   [".sha256", "text/plain"],
   [".sha512", "text/plain"],
-  [".txt", "text/plain"],
 ]);
 
 export function getMimeType(filePath: string, typeInBucket?: string): string {
   if (typeInBucket !== undefined && typeInBucket !== "application/octet-stream") return typeInBucket;
   const parsed = path.extname(filePath);
-  return knownMimeTypes.get(parsed) || "application/octet-stream";
+  return knownMimeTypes.get(parsed) || mime.getMimeType(filePath) || "application/octet-stream";
 }
 
 export async function getFiles(bucket: R2Bucket, currentPath: string): Promise<ScanListResult> {
