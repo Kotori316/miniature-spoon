84a21628a9cef915d09f0448c4b00a50c433f9cf
Kotori316 <Kotori316@gmail.com>
2024-05-05 13:19:11 +0900

    Setup SSG for contents page

---
+++ ssg/src/components.tsx
@@ -0,0 +1,122 @@
+import { html } from "hono/html";
+import type { FC, PropsWithChildren } from "hono/jsx";
+import { type PathObject, pathPairSort } from "./file";
+
+const Page: FC<PropsWithChildren<{ title: string }>> = (props) => {
+  const content = (
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <title>{props.title}</title>
+        <link href="/static/output.css" rel="stylesheet" />
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+  return html`<!doctype html>${content}`;
+};
+
+export function createRepositoryIndex(repositoryPathes: string[]) {
+  return (
+    <Page title="Repositories">
+      <div class="m-4">
+        <h1 class="font-sans text-2xl">List of Repositories</h1>
+        <div class="my-4 border-b-2 border-dashed border-indigo-500" />
+        <ul class="list-disc list-inside flex flex-col gap-1">
+          {repositoryPathes.map((path) => {
+            const lastSlash = path.lastIndexOf("/");
+            const group = path.substring(0, lastSlash).replaceAll("/", ".");
+            const name = path.substring(lastSlash + 1);
+            return (
+              <li>
+                <a
+                  class="font-mono underline-offset-auto text-emerald-700 hover:text-indigo-700 hover:underline decoration-indigo-400"
+                  href={path}
+                >
+                  {`${group}:${name}`}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Page>
+  );
+}
+
+const DirContents: FC<{
+  files: PathObject[];
+  dirs: PathObject[];
+  prefix: string;
+  directoryPagePrefix: string;
+}> = (props) => {
+  const { files, dirs, prefix, directoryPagePrefix } = props;
+  const sortedDirs = dirs.sort(pathPairSort);
+  const sortedFiles = files.sort(pathPairSort);
+
+  return (
+    <Page title={`Maven ${prefix || "Root"}`}>
+      <div class="m-4">
+        <h1 class="font-sans text-2xl">Index of {prefix}</h1>
+        <div class="my-4 border-b-2 border-dashed border-indigo-500" />
+        <div class="flex flex-col gap-3">
+          <div class="grid grid-cols-6 gap-2">
+            <div class="col-span-3 text-lg">File</div>
+            <div class="col-auto text-lg text-right">Size</div>
+            <div class="col-auto" />
+            <div class="col-auto text-lg">CreatedAt</div>
+          </div>
+          <Files
+            files={sortedDirs}
+            postfix="/"
+            pagePrefix={directoryPagePrefix || ""}
+          />
+          <Files files={sortedFiles} postfix="" pagePrefix="" />
+        </div>
+      </div>
+    </Page>
+  );
+};
+
+const Files: FC<{ files: PathObject[]; postfix: string; pagePrefix: string }> =
+  (props) => {
+    const { files, postfix, pagePrefix } = props;
+    return (
+      <div>
+        {files.map((o) => {
+          const nextPath = (pagePrefix || "") + o.absolutePath;
+          return (
+            <div class="grid grid-cols-6 gap-2">
+              <div class="col-span-3">
+                <a
+                  href={nextPath}
+                  class="font-mono text-emerald-700 hover:text-indigo-700 visited:text-purple-700"
+                >
+                  {o.basename + postfix}
+                </a>
+              </div>
+              <div class="col-auto text-right">{o.size}</div>
+              <div class="col-auto" />
+              <div class="col-auto">{o.created}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+export function createDirContents(
+  files: PathObject[],
+  dirs: PathObject[],
+  prefix: string,
+  directoryPagePrefix: string,
+) {
+  return (
+    <DirContents
+      dirs={dirs}
+      files={files}
+      prefix={prefix}
+      directoryPagePrefix={directoryPagePrefix}
+    />
+  );
+}
--- ssg/src/index.tsx
+++ ssg/src/index.tsx
@@ -1,91 +1,59 @@
-import {
-  ListObjectsV2Command,
-  type ListObjectsV2CommandInput,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import path from "node:path";
 import * as dotenv from "dotenv";
 import { Hono } from "hono";
-import { html } from "hono/html";
-import type { FC, PropsWithChildren } from "hono/jsx";
+import { ssgParams } from "hono/ssg";
+import { createDirContents, createRepositoryIndex } from "./components";
+import * as file from "./file";
 
 const app = new Hono();
 
 dotenv.config();
-const s3Client = new S3Client({
-  region: "auto",
-  endpoint: process.env.R2_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY ?? "",
-    secretAccessKey: process.env.R2_SECRET_KEY ?? "",
-  },
-});
 
-const option: ListObjectsV2CommandInput = {
-  Bucket: process.env.R2_BUCKET,
-};
-let list = await s3Client.send(new ListObjectsV2Command(option));
-const objects = list.Contents ?? [];
-while (list.IsTruncated) {
-  list = await s3Client.send(
-    new ListObjectsV2Command({
-      ...option,
-      ContinuationToken: list.NextContinuationToken,
-    }),
-  );
-  objects.push(...(list.Contents ?? []));
-}
-const repositoryPathes = objects
-  .map((o) => o.Key || "")
-  .filter((o) => o !== "")
+const objects = await file.getObjectsInR2();
+const objectKeys = objects.map((o) => o.Key || "").filter((o) => o !== "");
+
+const repositoryPathes = objectKeys
   .filter((o) => o.endsWith("maven-metadata.xml"))
   .filter((o) => !o.includes("SNAPSHOT"))
   .map((o) => o.replace("/maven-metadata.xml", ""));
 console.log(repositoryPathes);
 
+// Repository index
 app.get("/", (c) => {
   return c.html(createRepositoryIndex(repositoryPathes));
 });
 
-const Page: FC<PropsWithChildren<{ title: string }>> = (props) => {
-  const content = (
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <title>{props.title}</title>
-        <link href="/static/output.css" rel="stylesheet" />
-      </head>
-      <body>{props.children}</body>
-    </html>
-  );
-  return html`<!doctype html>${content}`;
-};
+// Each directories
+const directoryComponents = file.getDirectoryComponents(objects);
 
-function createRepositoryIndex(repositoryPathes: string[]) {
-  return (
-    <Page title="Repositories">
-      <div class="m-4">
-        <h1 class="font-sans text-2xl">List of Repositories</h1>
-        <div class="my-4 border-b-2 border-dashed border-indigo-500" />
-        <ul class="list-disc list-inside flex flex-col gap-1">
-          {repositoryPathes.map((path) => {
-            const lastSlash = path.lastIndexOf("/");
-            const group = path.substring(0, lastSlash).replaceAll("/", ".");
-            const name = path.substring(lastSlash + 1);
-            return (
-              <li>
-                <a
-                  class="font-mono underline-offset-auto text-emerald-700 hover:text-indigo-700 hover:underline decoration-indigo-400"
-                  href={path}
-                >
-                  {`${group}:${name}`}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </Page>
-  );
-}
+app.get(
+  "/:prefix{.+$}",
+  ssgParams(async () =>
+    Array.from(directoryComponents.keys()).map((o) => {
+      return {
+        prefix: o,
+      };
+    }),
+  ),
+  async (c) => {
+    const { prefix } = c.req.param();
+    const normalized = path.resolve("/", prefix);
+    const component = directoryComponents.get(normalized);
+    if (!component) {
+      console.log(
+        `Component for directory(${normalized}) is not found. ${c.req.path}`,
+      );
+      return c.notFound();
+    }
+    return c.html(
+      createDirContents(
+        component.files,
+        component.dirs,
+        component.directory,
+        process.env.DIRECTORY_PAGE_PREFIX ?? "",
+      ),
+    );
+  },
+);
 
 export default app;
