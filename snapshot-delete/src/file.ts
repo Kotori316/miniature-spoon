c3647d1aac60ea2cd490eabecd285facbd7ad446
Kotori316 <Kotori316@gmail.com>
2024-05-12 10:42:54 +0900

    Migrate to S3 API to run locally

--- snapshot-delete/src/file.ts
+++ snapshot-delete/src/file.ts
@@ -1,20 +1,49 @@
 import * as path from "node:path";
+import {
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  ListObjectsV2Command,
+  type ListObjectsV2CommandInput,
+  type S3Client,
+  type _Object as S3Object,
+} from "@aws-sdk/client-s3";
 import { XMLParser } from "fast-xml-parser";
+
+export interface S3Bucket {
+  s3Client: S3Client;
+  bucketName: string;
+}
+
 function resolve(parent: string, child: string): string {
   return path.relative("/", path.resolve(parent, child));
 }
 
-export async function deleteSnapshots(bucket: R2Bucket) {
-  const metadataFiles = await getFiles(bucket, (o) =>
-    o.key.endsWith("SNAPSHOT/maven-metadata.xml"),
+export async function deleteSnapshots(bucket: S3Bucket) {
+  const metadataFiles = await getFiles(
+    bucket,
+    (o) => o.Key?.endsWith("SNAPSHOT/maven-metadata.xml") || false,
   );
+  console.log("Found metadata", metadataFiles.length);
   const prefixes = (
     await Promise.all(
       metadataFiles.map(async (o) => {
-        const obj = await bucket.get(o.key);
-        if (!obj) return [];
-        const content = await obj.text();
-        const directory = path.dirname(o.key);
+        const obj = await bucket.s3Client.send(
+          new GetObjectCommand({
+            Bucket: bucket.bucketName,
+            Key: o.Key,
+          }),
+        );
+        if (!obj) {
+          console.log("Skip", o.Key, "as no file is found");
+          return [];
+        }
+        const content = await obj.Body?.transformToString();
+        if (!content) {
+          console.log("Skip", o.Key, "as file is empty");
+          return [];
+        }
+        const directory = path.dirname(o.Key ?? "");
+        console.log("Reading prefixed as", directory);
         return {
           prefix: directory,
           files: getLatestFilePrefixes(content).map((s) =>
@@ -24,6 +53,7 @@ export async function deleteSnapshots(bucket: R2Bucket) {
       }),
     )
   ).flatMap((s) => s) satisfies object[];
+  console.log("To be left", prefixes);
 
   const deletedFiles = (
     await Promise.all(
@@ -32,31 +62,51 @@ export async function deleteSnapshots(bucket: R2Bucket) {
       }),
     )
   ).flatMap((s) => s) satisfies string[];
+  console.log("Deleted", deletedFiles.length);
 
   return {
-    metadataFiles: metadataFiles.map((o) => o.key),
+    metadataFiles: metadataFiles.map((o) => o.Key),
     prefixes: prefixes,
     deletedFiles: deletedFiles,
   };
 }
 
 async function getFiles(
-  bucket: R2Bucket,
-  filter: (obj: R2Object) => boolean,
+  bucket: S3Bucket,
+  filter: (obj: S3Object) => boolean,
   prefix?: string,
-): Promise<R2Object[]> {
-  const options: R2ListOptions = {
-    prefix: prefix,
+): Promise<S3Object[]> {
+  const s3AccessStart = Date.now();
+  const options: ListObjectsV2CommandInput = {
+    Bucket: bucket.bucketName,
+    Prefix: prefix,
   };
-  let list = await bucket.list(options);
-  const objects = list.objects.filter(filter);
-  while (list.truncated) {
-    list = await bucket.list({
-      ...options,
-      cursor: list.cursor,
-    });
-    objects.push(...list.objects.filter(filter));
+  let list = await bucket.s3Client.send(new ListObjectsV2Command(options));
+  const objects = list.Contents?.filter(filter) ?? [];
+  console.log(
+    `Got ${objects.length} items in ${
+      Date.now() - s3AccessStart
+    }ms. Prefix: ${prefix}`,
+  );
+  while (list.IsTruncated) {
+    list = await bucket.s3Client.send(
+      new ListObjectsV2Command({
+        ...options,
+        ContinuationToken: list.NextContinuationToken,
+      }),
+    );
+    objects.push(...(list.Contents?.filter(filter) ?? []));
+    console.log(
+      `Got ${objects.length} items in ${
+        Date.now() - s3AccessStart
+      }ms. Prefix: ${prefix}`,
+    );
   }
+  console.log(
+    `Total ${objects.length} items in ${
+      Date.now() - s3AccessStart
+    }ms. Prefix: ${prefix}`,
+  );
   return objects;
 }
 
@@ -85,19 +135,36 @@ function getLatestFilePrefixes(xmlText: string): string[] {
 }
 
 async function deleteFiles(
-  bucket: R2Bucket,
+  bucket: S3Bucket,
   prefix: string,
   leaveFilePrefixes: string[],
 ): Promise<string[]> {
+  const dryRun: boolean = process.env.DRY_RUN === "true";
+
   const snapshotFiles = await getFiles(
     bucket,
-    (o) => !leaveFilePrefixes.some((allowed) => o.key.startsWith(allowed)),
+    (o) => !leaveFilePrefixes.some((allowed) => o.Key?.startsWith(allowed)),
     prefix,
   );
 
-  const keys = snapshotFiles.map((o) => o.key);
+  const keys = snapshotFiles.flatMap((o) => (o.Key ? [{ Key: o.Key }] : []));
+  const total: string[] = [];
 
-  bucket.delete(keys);
+  while (keys.length > 0) {
+    const chunk = keys.splice(0, 1000);
+    if (!dryRun) {
+      await bucket.s3Client.send(
+        new DeleteObjectsCommand({
+          Bucket: bucket.bucketName,
+          Delete: { Objects: chunk },
+        }),
+      );
+      console.log("Deleted", chunk.length, "files");
+    } else {
+      console.log("Dry run.", "Delete", chunk.length, "files");
+    }
+    total.push(...chunk.map((t) => t.Key));
+  }
 
-  return keys;
+  return total;
 }
--- snapshot-delete/src/index.ts
+++ snapshot-delete/src/index.ts
@@ -1,33 +1,30 @@
+import { S3Client } from "@aws-sdk/client-s3";
 import { deleteSnapshots } from "./file";
 
-export default {
-  // The scheduled handler is invoked at the interval set in our wrangler.toml's
-  // [[triggers]] configuration.
-  async scheduled(
-    _event: ScheduledEvent,
-    env: Env,
-    _ctx: ExecutionContext,
-  ): Promise<void> {
-    deleteSnapshots(env.MAVEN_BUCKET);
-  },
+async function main() {
+  if (
+    !process.env.R2_ENDPOINT ||
+    !process.env.R2_ACCESS_KEY ||
+    !process.env.R2_SECRET_KEY ||
+    !process.env.R2_BUCKET
+  ) {
+    console.log("Not all required env values are set.");
+    process.exit(1);
+  }
+  const s3Client = new S3Client({
+    region: "auto",
+    endpoint: process.env.R2_ENDPOINT ?? "",
+    credentials: {
+      accessKeyId: process.env.R2_ACCESS_KEY ?? "",
+      secretAccessKey: process.env.R2_SECRET_KEY ?? "",
+    },
+  });
+  const bucketName = process.env.R2_BUCKET ?? "";
 
-  async fetch(
-    request: Request,
-    env: Env,
-    _ctx: ExecutionContext,
-  ): Promise<Response> {
-    if ((env.ENVIRONMENT as string) === "local") {
-      console.log("Coming request", request.method, request.url);
-      const result = await deleteSnapshots(env.MAVEN_BUCKET);
-      return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
-    return new Response(undefined, {
-      status: 404,
-    });
-  },
-};
+  await deleteSnapshots({
+    s3Client,
+    bucketName,
+  });
+}
+
+await main();
