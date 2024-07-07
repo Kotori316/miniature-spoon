12022289d270e2a727aed82158aa2ad380fa072b
Kotori316 <Kotori316@gmail.com>
2024-07-07 15:26:36 +0900

    Scheduler for copy task

--- reposilite-server-terraform/copy_object.tf
+++ reposilite-server-terraform/copy_object.tf
@@ -1,4 +1,3 @@
-
 resource "terraform_data" "trigger_bucket" {
   input = google_storage_bucket.maven_bucket.name
 }
@@ -155,4 +154,106 @@ data "google_secret_manager_secret" "cloudflare_access_key" {
 
 data "google_secret_manager_secret" "cloudflare_secret_key" {
   secret_id = "cloudflare_secret_key"
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  location               = "us-central1"
+  repository_id          = "${var.base_name}-copy-job-registory"
+  description            = "Docker repository for ${var.base_name}"
+  format                 = "DOCKER"
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-1day"
+    action = "DELETE"
+    condition {
+      tag_state  = "ANY"
+      older_than = "${60 * 60 * 24}s"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 1
+    }
+  }
+}
+
+data "google_artifact_registry_docker_image" "repo" {
+  location      = google_artifact_registry_repository.repo.location
+  repository_id = google_artifact_registry_repository.repo.repository_id
+  image_name    = "copy_task:latest"
+}
+
+resource "google_cloud_run_v2_job" "copy_task" {
+  name     = "${var.base_name}-copy-job"
+  location = var.region
+
+  template {
+    template {
+      service_account = google_service_account.copy_flow_runner.email
+      containers {
+        image = data.google_artifact_registry_docker_image.repo.self_link
+        resources {
+          limits = {
+            cpu    = "1"
+            memory = "512Mi"
+          }
+        }
+        env {
+          name  = "AWS_REGION"
+          value = "auto"
+        }
+        env {
+          name = "AWS_ENDPOINT_URL_S3"
+          value_source {
+            secret_key_ref {
+              secret  = data.google_secret_manager_secret.cloudflare_s3_endpoint.id
+              version = "latest"
+            }
+          }
+        }
+        env {
+          name = "AWS_ACCESS_KEY_ID"
+          value_source {
+            secret_key_ref {
+              secret  = data.google_secret_manager_secret.cloudflare_access_key.id
+              version = "latest"
+            }
+          }
+        }
+        env {
+          name = "AWS_SECRET_ACCESS_KEY"
+          value_source {
+            secret_key_ref {
+              secret  = data.google_secret_manager_secret.cloudflare_secret_key.id
+              version = "latest"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+locals {
+  job_run_url = "https://${google_cloud_run_v2_job.copy_task.location}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${google_cloud_run_v2_job.copy_task.project}/jobs/${google_cloud_run_v2_job.copy_task.name}:run"
+}
+
+resource "google_cloud_scheduler_job" "copy_task" {
+  name        = "${google_cloud_run_v2_job.copy_task.name}-scheduler-trigger"
+  description = "Scheduled trigger for ${google_cloud_run_v2_job.copy_task.name}"
+  region      = google_cloud_run_v2_job.copy_task.location
+  schedule    = "20 3 * * *"
+  time_zone   = "Asia/Tokyo"
+
+  http_target {
+    http_method = "POST"
+    uri         = local.job_run_url
+    oauth_token {
+      service_account_email = google_service_account.copy_flow_runner.email
+      scope                 = "https://www.googleapis.com/auth/cloud-platform"
+    }
+  }
+  depends_on = [google_cloud_run_v2_job_iam_member.copy_task]
 }
