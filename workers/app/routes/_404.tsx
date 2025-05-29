a9de640f7de780fb38b89fa48abaaac804b5b0b5
Kotori316 <Kotori316@gmail.com>
2025-05-29 19:47:59 +0900

    Change 404 page style

--- workers/app/routes/_404.tsx
+++ workers/app/routes/_404.tsx
@@ -1,8 +1,18 @@
 import type { NotFoundHandler } from "hono";
+import { css } from "hono/css";
 
 const handler: NotFoundHandler = (c) => {
   c.status(404);
-  return c.render("404 Not Found");
+  return c.render(
+    <div class={styles}>
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>,
+  );
 };
+
+const styles = css`
+  padding: 1rem;
+`;
 
 export default handler;
